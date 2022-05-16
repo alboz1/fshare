7b08e51317159412d69e4efce20f54b99b03386f
alboz1 <vucajjuerd@gmail.com>
2022-05-16 17:27:37 +0200

    styled file info, added animating loader when uploading

--- client/assets/main.js
+++ client/assets/main.js
@@ -1,25 +1,47 @@
 const form = document.querySelector('#upload-form');
 const submitBtn = document.querySelector('input[type="submit"]');
+const upploadLoader = form.querySelector('.uploading-loader-container');
 
 form.addEventListener('submit', (e) => {
     console.log(e);
     submitBtn.setAttribute('disabled', 'true');
+    upploadLoader.style.display = 'flex';
 });
 
 const fileUpload = document.querySelector('input[type="file"]');
 const img = document.querySelector('#file-info-picture');
 const figureCaption = document.querySelector('.figure-caption');
+const fileInfo = document.querySelector('.file-info-wrapper');
+const wrapper = form.querySelector('.wrapper');
+const svg = document.querySelector('#file-icon');
+const loader = wrapper.querySelector('.loading-file');
 
 fileUpload.addEventListener('change', (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
-
+    fileInfo.classList.remove('show');
+    img.style.display = 'none';
+    svg.style.display = 'none';
+    wrapper.style.maxHeight = '0';
+    
     if (file) {
         reader.readAsDataURL(file);
-        reader.onloadend = () => {
-            console.log(reader.result);
-            img.setAttribute('src', reader.result);
-            figureCaption.textContent = file.name;
+        wrapper.style.maxHeight = '100%';
+            reader.onloadstart = () => {
+                loader.style.display = 'inline-block';
+            }
+            reader.onload = () => {
+                loader.style.display = 'none';
+
+                if (file.type.match(/image\/*/)) {
+                    img.style.display = 'inline-block';
+                    img.style.backgroundImage = `url(${reader.result})`;
+                } else {
+                    svg.style.display = 'inline-block';
+                }
+
+                figureCaption.textContent = file.name;
+                fileInfo.classList.add('show');
         }
     }
 });

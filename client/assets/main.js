a3f01818a08d49f7c66c18e12160e8f79b70d23f
alboz1 <vucajjuerd@gmail.com>
2022-05-27 16:01:53 +0200

    cleaned up code

--- client/assets/main.js
+++ client/assets/main.js
@@ -1,56 +1,81 @@
-const form = document.querySelector('#upload-form');
-const submitBtn = document.querySelector('button[type="submit"]');
-const submitBtnText = submitBtn.querySelector('.text');
-const btnLoader = submitBtn.querySelector('button[type="submit"] .loader');
+const app = {
+    init() {
+        controllers.selectFile();
+        controllers.submitForm();
 
-window.addEventListener("pageshow", e => {
-    if (e.persisted) {
-        window.location.reload();
-    }
-}, false);
-
-form.addEventListener('submit', e => {
-    btnLoader.style.display = 'inline-block';
-    submitBtnText.textContent = 'Uploading...';
-    submitBtn.setAttribute('disabled', 'true');
-});
-
-const fileUpload = document.querySelector('input[type="file"]');
-const img = document.querySelector('#file-info-picture');
-const figureCaption = document.querySelector('.figure-caption');
-const fileInfo = document.querySelector('.file-info-wrapper');
-const wrapper = form.querySelector('.wrapper');
-const svg = document.querySelector('#file-icon');
-const loader = wrapper.querySelector('.loading-file');
-
-fileUpload.addEventListener('change', e => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    fileInfo.classList.remove('show');
-    img.style.display = 'none';
-    svg.style.display = 'none';
-    wrapper.style.maxHeight = '0';
-
-    if (file) {
-        reader.readAsDataURL(file);
-        wrapper.style.maxHeight = '100%';
-
-        reader.onloadstart = () => {
-            loader.style.display = 'block';
-        }
-
-        reader.onload = () => {
-            loader.style.display = 'none';
-
-            if (file.type.match(/image\/.*/)) {
-                img.style.display = 'inline-block';
-                img.style.backgroundImage = `url(${reader.result})`;
-            } else {
-                svg.style.display = 'inline-block';
+        window.addEventListener("pageshow", e => {
+            if (e.persisted) {
+                window.location.reload();
             }
-
-            figureCaption.textContent = file.name;
-            fileInfo.classList.add('show');
-        }
+        }, false);
     }
-});
+};
+
+const controllers = {
+    submitForm() {
+        const form = document.querySelector('#upload-form');
+        form.addEventListener('submit', view.showUploadingAnimation);
+    },
+
+    selectFile() {
+        const fileUpload = document.querySelector('input[type="file"]');
+        fileUpload.addEventListener('change', e => {
+            const file = e.target.files[0];
+            view.showSelectedFile(file);
+        });
+    }
+};
+
+const view = {
+    showSelectedFile(file) {
+        const wrapper = document.querySelector('.wrapper');
+        const loader = wrapper.querySelector('.loading-file');
+        const fileInfo = wrapper.querySelector('.file-info-wrapper');
+        const img = fileInfo.querySelector('#file-info-picture');
+        const svg = fileInfo.querySelector('#file-icon');
+        const figureCaption = fileInfo.querySelector('.figure-caption');
+
+        const reader = new FileReader();
+
+        //reset
+        fileInfo.classList.remove('show');
+        img.style.display = 'none';
+        svg.style.display = 'none';
+        wrapper.style.maxHeight = '0';
+
+        if (file) {
+            reader.readAsDataURL(file);
+            wrapper.style.maxHeight = '100%';
+
+            reader.onloadstart = () => {
+                loader.style.display = 'block';
+            }
+    
+            reader.onload = () => {
+                loader.style.display = 'none';
+    
+                if (file.type.match(/image\/.*/)) {
+                    img.style.display = 'inline-block';
+                    img.style.backgroundImage = `url(${reader.result})`;
+                } else {
+                    svg.style.display = 'inline-block';
+                }
+    
+                figureCaption.textContent = file.name;
+                fileInfo.classList.add('show');
+            }
+        }
+    },
+
+    showUploadingAnimation() {
+        const submitBtn = document.querySelector('button[type="submit"]');
+        const submitBtnText = submitBtn.querySelector('.text');
+        const btnLoader = submitBtn.querySelector('.loader');
+
+        btnLoader.style.display = 'inline-block';
+        submitBtnText.textContent = 'Uploading...';
+        submitBtn.setAttribute('disabled', 'true');
+    }
+};
+
+app.init();
